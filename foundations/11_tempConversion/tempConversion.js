676979389aa530473d1bf3c73252070803881caa
Gabriel <med.gabrielgarcia@gmail.com>
2026-08-17 01:17:28 -0500

    Finish the exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  let result = (faren - 32)/ 1.8;
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let result = (cels * 1.8) + 32;
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line

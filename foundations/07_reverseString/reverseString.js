857d074411218178a4036600d2c2a8ff5d35ce92
Gabriel <med.gabrielgarcia@gmail.com>
2026-08-16 19:08:54 -0500

    Finish the exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let string = str;
+    str = str.split("");
+    str = str.reverse();
+    str = str.join("");
+    return str;
 };
 
 // Do not edit below this line

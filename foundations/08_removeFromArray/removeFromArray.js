f027f9552fa500f0313c486831ae47194184e2c8
Gabriel <med.gabrielgarcia@gmail.com>
2026-08-16 23:02:09 -0500

    Finish the exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    for(let i = 0; i < arg.length; i++){
+        while(arr.indexOf(arg[i]) != -1){
+            let remove = arr.indexOf(arg[i]);
+            arr.splice(remove, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line

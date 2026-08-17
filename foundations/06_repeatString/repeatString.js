9854a7302a9865da49e013be8dc92adcc72f567b
Gabriel <med.gabrielgarcia@gmail.com>
2026-08-16 19:01:44 -0500

    Finish the exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num){ 
+    if(num < 0){
+        return "ERROR"
+    } else { 
+         string = ""; 
 
-};
+        for(let i = 0; i < num; i++){ 
+            string += str; 
+        } 
+
+        return string
+    };
+}; 
+
 
 // Do not edit below this line
 module.exports = repeatString;

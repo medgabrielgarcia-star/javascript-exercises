9ac02da84993a5825a4f7a2457c1755945820642
Gabriel <med.gabrielgarcia@gmail.com>
2026-08-17 00:45:08 -0500

    Finish the exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(min, max) {
+    let result = 0;
+    let inicio = Math.min(min, max);
+    let final = Math.max(min, max);
+    if(typeof min != "number" || typeof max != "number"){
+        return "ERROR"
+    } else if(!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR"
+    } else if(min < 0 || max <0) {
+        return "ERROR"
+    } else {for(let i = inicio; i <= final; i++) {
+        result+=i
+    } return result }
+}
 // Do not edit below this line
 module.exports = sumAll;
